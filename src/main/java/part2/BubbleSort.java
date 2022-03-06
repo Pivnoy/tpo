63ce02eb4bf606e3feedbf702aac24efed65f2bd
Евгений Макарьев <evgenij_makarev20021@mail.ru>
2022-03-06 16:39:34 +0300

    Fix netsing terms

--- src/main/java/part2/BubbleSort.java
+++ src/main/java/part2/BubbleSort.java
@@ -2,18 +2,26 @@ package part2;
 
 public class BubbleSort {
 
-    public void bubbleSort(int [] arr) {
-        if (arr != null) {
-            int n = arr.length;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n - i - 1; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        int t = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = t;
-                    }
+    private void swap(int [] arr, int j){
+        int t = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = t;
+    }
+
+    private void bubbleSortImpl(int [] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j);
                 }
             }
+        }
+    }
+
+    public void bubbleSort(int [] arr){
+        if (arr != null){
+            bubbleSortImpl(arr);
         }
     }
 }
